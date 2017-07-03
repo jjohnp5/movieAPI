7fde5d5886ce04f9528db2a53ef4ce023d292836
John <jjohnp5@gmail.com>
2017-07-03 16:17:13 -0500

    added new features, body-parser, search feature

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var request = require('request');
+var bodyParser = require('body-parser');
 
 app.set("views", path.join(__dirname, '/views'));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
    res.render("landing");
 });
 app.get("/movies", function(req, res){
-   res.render("movies");
+    request("https://api.themoviedb.org/3/movie/top_rated?api_key=f2542b8b01898f42918c9addc4ed07a0&language=en-US&page=1", function (error, response, body){
+        var result = JSON.parse(body);
+        res.render("movies", {result: result});
+    });
+
+});
+app.post("/movies/search", function (req, res) {
+    var query = req.body.query;
+    var queryArr = query.split(" ");
+    var keyword = "";
+    for(var i = 0; i < queryArr.length; i++){
+        if (i === 0 ){
+            keyword = queryArr[i];
+        }else{
+            var base = "%20" + queryArr[i];
+            keyword += base;
+        }
+    }
+    console.log(keyword);
+    request("https://api.themoviedb.org/3/search/movie?api_key=f2542b8b01898f42918c9addc4ed07a0&language=en-US&query="+keyword, function(error, response, body){
+        var result = JSON.parse(body);
+       res.render("search", {result: result})
+    });
 });
 app.get("/movies/popular", function (req, res) {
     request("https://api.themoviedb.org/3/movie/popular?api_key=f2542b8b01898f42918c9addc4ed07a0", function (error, response, body) {
--- public/main.js
+++ public/main.js
@@ -2,11 +2,3 @@
  * Created by John on 6/29/2017.
  */
 
-$(document).ready(function() {
-    // Gets the span width of the filled-ratings span
-    // this will be the same for each rating
-    var star_rating_width = $('.fill-ratings span').width();
-    // Sets the container of the ratings to span width
-    // thus the percentages in mobile will never be wrong
-    $('.star-ratings').width(star_rating_width);
-});
